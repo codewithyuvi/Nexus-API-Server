a506ef4cf51d91a0a673894f0f3a0ceda0cc1f3b
codewithyuvi <yuvrajbansal30dec@gmail.com>
2026-06-09 16:28:15 +0530

    Add billing queue with hourly synchronization using BullMQ

---
+++ src/queues/billing.queue.ts
@@ -0,0 +1,6 @@
+import { Queue } from "bullmq";
+import { redisConnection } from "../constants/redisConnection";
+
+export const billingQueue = new Queue('BillingQueue', {connection: redisConnection});
+
+billingQueue.add('hourly-sync', {}, {repeat: {pattern: '0 * * * *'} })
